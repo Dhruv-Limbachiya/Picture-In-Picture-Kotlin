ac2b91cff2c0fcefd3a1b27c1c064d88be5d655c
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-11-20 12:18:43 +0530

    youtube picture in picture mode.

--- app/src/main/java/com/example/pipmodesample/YouTubeActivity.kt
+++ app/src/main/java/com/example/pipmodesample/YouTubeActivity.kt
@@ -1,24 +1,18 @@
 package com.example.pipmodesample
 
 import android.app.PictureInPictureParams
-import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.util.Rational
-import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.core.view.isVisible
 import com.example.pipmodesample.databinding.ActivityYouTubeBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.youtube.player.YouTubeBaseActivity
-import com.google.android.youtube.player.YouTubeInitializationResult
-import com.google.android.youtube.player.YouTubePlayer
 
 class YouTubeActivity : YouTubeBaseActivity() {
 
     private lateinit var mBinding: ActivityYouTubeBinding
-    private var player: YouTubePlayer? = null
 
     private var mPictureInPictureParams: PictureInPictureParams.Builder? = null
 
@@ -30,31 +24,6 @@ class YouTubeActivity : YouTubeBaseActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             mPictureInPictureParams = PictureInPictureParams.Builder()
         }
-
-
-        mBinding.ytPlayer.initialize(getString(R.string.api_key),
-            object : YouTubePlayer.OnInitializedListener {
-                override fun onInitializationSuccess(
-                    p0: YouTubePlayer.Provider?,
-                    youtubeplayer: YouTubePlayer?,
-                    p2: Boolean
-                ) {
-                    if (youtubeplayer != null) {
-                        player = youtubeplayer
-                        youtubeplayer.loadVideo("62JB64aQC9A")
-                        youtubeplayer.play()
-                    }
-                }
-
-                override fun onInitializationFailure(
-                    p0: YouTubePlayer.Provider?,
-                    p1: YouTubeInitializationResult?
-                ) {
-                    Toast.makeText(this@YouTubeActivity, "Video player Failed", Toast.LENGTH_SHORT)
-                        .show();
-
-                }
-            })
 
     }
 
@@ -74,7 +43,8 @@ class YouTubeActivity : YouTubeBaseActivity() {
     private fun playInPictureInPictureMode() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Aspect ratio for PIP mode screen.
-            val aspectRatio = Rational(mBinding.ytPlayer.width, mBinding.ytPlayer.height)
+            val aspectRatio =
+                Rational(mBinding.youtubePlayerView.width, mBinding.youtubePlayerView.height)
 
             mPictureInPictureParams?.setAspectRatio(aspectRatio)?.build()
 
@@ -94,22 +64,4 @@ class YouTubeActivity : YouTubeBaseActivity() {
 
     }
 
-    override fun onPictureInPictureModeChanged(
-        isInPictureInPictureMode: Boolean,
-        newConfig: Configuration?
-    ) {
-        // handle ui on picture in picture mode change.
-        if (isInPictureInPictureMode) {
-            player?.play()
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        player?.play()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
 }
